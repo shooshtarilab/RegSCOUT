db99241e61080db9467a7a283ef6ba03b5295eb9
Richard Zhang <richardzhang@Richards-MacBook-Pro.local>
2025-09-03 12:02:12 -0400

    Updated bash script to include new parameters, Hi-C and eQTL can now be requested separately

--- bash_scripts/RegSCOUT_main.sh
+++ bash_scripts/RegSCOUT_main.sh
@@ -6,23 +6,24 @@ while [[ "$#" -gt 0 ]]; do
     --output_dir) output_dir="$2"; shift ;;
     --mode) mode="$2"; shift ;;
     --finemap) finemap="$2"; shift ;;
-    --hic_eqtl_analysis) hic_eqtl_analysis="$2"; shift;;
+    --hic_analysis) hic_analysis="$2"; shift;;
+    --eqtl_analysis) eqtl_analysis="$2"; shift;;
     --histone_mark_analysis) histone_mark_analysis="$2"; shift;;
-    --snp_ref) snp_ref="$2"; shift;;
+    --snp_ref_dir) snp_ref_dir="$2"; shift;;
     --population) population="$2"; shift;;
-    --sum_stats) sum_stats="$2"; shift;;
-    --lead_snps) lead_snps="$2"; shift;;
-    --plink2_bin) plink2_bin="$2"; shift;;
+    --sum_stats_dir) sum_stats_dir="$2"; shift;;
+    --lead_snps_dir) lead_snps_dir="$2"; shift;;
+    --plink2_dir) plink2_dir="$2"; shift;;
     --sample_num) sample_num="$2"; shift;;
     --locus_region) locus_region="$2"; shift ;;
     --ld_th) ld_th="$2"; shift ;;
-    --fgwas_src) fgwas_src="$2"; shift;;
+    --fgwas_dir) fgwas_dir="$2"; shift;;
     --ci_th) ci_th="$2"; shift;;
     --ci_ppa_th) ci_ppa_th="$2"; shift;;
-    --jaspar_mtx) jaspar_mtx="$2"; shift ;;
+    --jaspar_mtx_dir) jaspar_mtx_dir="$2"; shift ;;
     --ci_gwas_dir) ci_gwas_dir="$2"; shift;;
     --genome_build) genome_build="$2"; shift ;;
-    --seurat_obj) seurat_obj="$2"; shift ;;
+    --seurat_obj_dir) seurat_obj_dir="$2"; shift ;;
     --cell_count_th) cell_count_th="$2"; shift;;
     --peak_th) peak_th="$2"; shift;;
     --coaccess_th) coaccess_th="$2"; shift;;
@@ -44,20 +45,20 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 if [ "$finemap" == "Y" ]; then
-    Rscript fgwas_data_prep.R --output_dir "$output_dir" --snp_ref "$snp_ref" --population "$population" --sum_stats "$sum_stats" --lead_snps "$lead_snps" --plink2_bin "$plink2_bin" --sample_num "$sample_num" --locus_region "$locus_region" --ld_th "$ld_th"
-    Rscript fine_map.R --output_dir "$output_dir" --fgwas_src "$fgwas_src" --ci_th "$ci_th" --ci_ppa_th "$ci_ppa_th"
+    Rscript fgwas_data_prep.R --output_dir "$output_dir" --snp_ref_dir "$snp_ref_dir" --population "$population" --sum_stats_dir "$sum_stats_dir" --lead_snps_dir "$lead_snps_dir" --plink2_dir "$plink2_dir" --sample_num "$sample_num" --locus_region "$locus_region" --ld_th "$ld_th"
+    Rscript fine_map.R --output_dir "$output_dir" --fgwas_dir "$fgwas_dir" --ci_th "$ci_th" --ci_ppa_th "$ci_ppa_th"
     suff="gwas_CI.txt"
     ci_gwas_dir="${output_dir}${suff}"  
 fi
 
 if [ "$mode" == "ATAC_obj" ]; then
-    Rscript EffectSNP.R --output_dir "$output_dir" --jaspar_mtx "$jaspar_mtx" --ci_gwas_dir "$ci_gwas_dir" --genome_build "$genome_build"
-    Rscript Peak_cell_extract.R --seurat_obj "$seurat_obj" --output_dir "$output_dir" --peak_th "$peak_th" --cell_count_th "$cell_count_th"
-    Rscript peak_interaction_extract.R --genome_build "$genome_build" --seurat_obj "$seurat_obj" --output_dir "$output_dir" --coaccess_th "$coaccess_th" --cic_genomic_window "$cic_genomic_window" --peak_th "$peak_th"
+    Rscript EffectSNP.R --output_dir "$output_dir" --jaspar_mtx_dir "$jaspar_mtx_dir" --ci_gwas_dir "$ci_gwas_dir" --genome_build "$genome_build"
+    Rscript Peak_cell_extract.R --seurat_obj_dir "$seurat_obj_dir" --output_dir "$output_dir" --peak_th "$peak_th" --cell_count_th "$cell_count_th"
+    Rscript peak_interaction_extract.R --genome_build "$genome_build" --seurat_obj_dir "$seurat_obj_dir" --output_dir "$output_dir" --coaccess_th "$coaccess_th" --cic_genomic_window "$cic_genomic_window" --peak_th "$peak_th"
     Rscript Peak_Gene_SNP_Integration.R --output_dir "$output_dir" --prom_th_up "$prom_th_up"  --prom_th_down "$prom_th_down" --gencode_dir "$gencode_dir"
 
 elif [ "$mode" == "peak_table" ]; then
-    Rscript EffectSNP.R --output_dir "$output_dir" --jaspar_mtx "$jaspar_mtx" --ci_gwas_dir "$ci_gwas_dir" --genome_build "$genome_build"
+    Rscript EffectSNP.R --output_dir "$output_dir" --jaspar_mtx_dir "$jaspar_mtx_dir" --ci_gwas_dir "$ci_gwas_dir" --genome_build "$genome_build"
     Rscript Peak_Gene_SNP_Integration.R --output_dir "$output_dir" --prom_th_up "$prom_th_up"  --prom_th_down "$prom_th_down" --gencode_dir "$gencode_dir"    
 fi
 
@@ -69,11 +70,14 @@ if [ "$histone_mark_analysis" == "Y" ]; then
     Rscript histone_mark_analysis.R --output_dir "$output_dir" --hist_mark_instruct_dir "$hist_mark_instruct_dir" 
 fi
 
-if [ "$hic_eqtl_analysis" == "Y" ]; then
+if [ "$hic_analysis" == "Y" ]; then
     Rscript HiC_Analysis.R --output_dir "$output_dir" --hic_instruct_dir "$hic_instruct_dir" --prom_th_up "$prom_th_up" --prom_th_down "$prom_th_down" --gencode_dir "$gencode_dir"
-    Rscript eQTL_Analysis.R --output_dir "$output_dir" --eqtl_instruct_dir "$eqtl_instruct_dir"
-    Rscript final_outputs.R --output_dir "$output_dir" --finemap "$finemap" --ci_gwas_dir "$ci_gwas_dir" --tf_score_th "$tf_score_th" --gene_score_th "$gene_score_th" --gene_sum_ppa_th "$gene_sum_ppa_th"
+fi 
 
-elif [[ -z "$hic_eqtl_analysis" && -z "$mode" ]]; then
+if [ "$eqtl_analysis" == "Y" ]; then
+    Rscript eQTL_Analysis.R --output_dir "$output_dir" --eqtl_instruct_dir "$eqtl_instruct_dir"
+fi 
+
+if [ -z "$mode" ]; then
     Rscript final_outputs.R --output_dir "$output_dir" --finemap "$finemap" --ci_gwas_dir "$ci_gwas_dir" --tf_score_th "$tf_score_th" --gene_score_th "$gene_score_th" --gene_sum_ppa_th "$gene_sum_ppa_th"
 fi
